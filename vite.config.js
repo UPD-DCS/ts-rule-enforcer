87a57d31445779decb0d064cf4c6a20c9a04b1ba
Juan Felipe Coronel <juanfelipe.coronel@gmail.com>
2025-03-16 11:24:56 +0800

    Fix Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  "server.allowedHosts": ["localhost", "cs12.upd-dcs.work"],
+  server: {
+    allowedHosts: ["localhost", "cs12.upd-dcs.work"],
+  },
 })
